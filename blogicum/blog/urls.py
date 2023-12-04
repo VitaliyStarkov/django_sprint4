fbf449d379556d92921bb43ccb647dbb81f36e7d
Старков Виталий <vetalsta@gmail.com>
2023-12-04 22:52:31 +0300

    fix 1st review

--- blogicum/blog/urls.py
+++ blogicum/blog/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
+
      path('', views.PostListView.as_view(),
           name='index'),
 
@@ -40,4 +41,4 @@ urlpatterns = [
 
      path('category/<slug:category_slug>/',
           views.CategoryListView.as_view(), name='category_posts')
-     ]
+]
